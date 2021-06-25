92672e90f61dcc1d1f79bbc3e53e9ae50579cea6
Ben <>
2021-06-25 16:45:32 +0700

    Add Class Admin, Flashsale, Transasction, DetailedTransaction

---
+++ src/model/Admin.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Benedict
+ */
+public class Admin extends Person {
+
+    public void createFlashsale(int idItem, int price, String endDate) {
+
+    }
+
+    public void editFlashsale(int idFlashsale) {
+        
+    }
+    
+    public void deleteFlashsale(int idFlashsale) {
+        
+    }
+    
+    public void deleteUser(int idUser) {
+        
+    }
+}
---
+++ src/model/Transaction.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Benedict
+ */
+public class Transaction {
+    private int idTransaction;
+    private int idBuyer;
+    private int idSeller;
+    private String purchaseDate;
+    private DeliveryStatus deliveryStatus;
+    private CourierType courierType;
+    private int courierPrice;
+    private int discount;
+    private int payAmount;
+
+    public int getIdTransaction() {
+        return idTransaction;
+    }
+
+    public void setIdTransaction(int idTransaction) {
+        this.idTransaction = idTransaction;
+    }
+
+    public int getIdBuyer() {
+        return idBuyer;
+    }
+
+    public void setIdBuyer(int idBuyer) {
+        this.idBuyer = idBuyer;
+    }
+
+    public int getIdSeller() {
+        return idSeller;
+    }
+
+    public void setIdSeller(int idSeller) {
+        this.idSeller = idSeller;
+    }
+
+    public String getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(String purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public DeliveryStatus getDeliveryStatus() {
+        return deliveryStatus;
+    }
+
+    public void setDeliveryStatus(DeliveryStatus deliveryStatus) {
+        this.deliveryStatus = deliveryStatus;
+    }
+
+    public CourierType getCourierType() {
+        return courierType;
+    }
+
+    public void setCourierType(CourierType courierType) {
+        this.courierType = courierType;
+    }
+
+    public int getCourierPrice() {
+        return courierPrice;
+    }
+
+    public void setCourierPrice(int courierPrice) {
+        this.courierPrice = courierPrice;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public int getPayAmount() {
+        return payAmount;
+    }
+
+    public void setPayAmount(int payAmount) {
+        this.payAmount = payAmount;
+    }
+    
+    
+}
