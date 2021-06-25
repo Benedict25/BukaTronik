e5636c4e20b1a63f8037650950871aa9bd7d6526
Matthew <>
2021-06-25 16:41:11 +0700

    Add Class Buyer, Wishlist, ItemInShoppingCart

---
+++ src/model/Wishlist.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Matthew
+ */
+public class Wishlist {
+    private int idPerson;
+    private int idItem;
+
+    public int getIdPerson() {
+        return idPerson;
+    }
+
+    public void setIdPerson(int idPerson) {
+        this.idPerson = idPerson;
+    }
+
+    public int getIdItem() {
+        return idItem;
+    }
+
+    public void setIdItem(int idItem) {
+        this.idItem = idItem;
+    }
+}
