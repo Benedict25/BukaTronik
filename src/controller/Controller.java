bbc74cfc71c19a4392b043f224064f5217249ce4
Ben <Intel@Trench>
2021-06-26 13:43:48 +0700

    Declare arraylist into controller

---
+++ src/controller/Controller.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.util.ArrayList;
+import model.Buyer;
+import model.Flashsale;
+import model.Seller;
+import model.Voucher;
+/**
+ *
+ * @author Benedict
+ */
+public class Controller {
+    static ArrayList<Buyer> buyer = new ArrayList();
+    static ArrayList<Seller> seller = new ArrayList();
+    static ArrayList<Flashsale> flashSale = new ArrayList();
+    static ArrayList<Voucher> voucher = new ArrayList();
+}
