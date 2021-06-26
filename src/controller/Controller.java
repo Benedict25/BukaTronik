6018d5b25d28d31272972b592d555f8b675f990b
Ignatius Dariel <>
2021-06-26 17:41:11 +0700

    Add Menu and Controller AddItem

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,17 +6,27 @@
 package controller;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import model.Buyer;
 import model.Flashsale;
 import model.Seller;
 import model.Voucher;
+
 /**
  *
  * @author Benedict
  */
 public class Controller {
+
     static ArrayList<Buyer> buyer = new ArrayList();
     static ArrayList<Seller> seller = new ArrayList();
     static ArrayList<Flashsale> flashSale = new ArrayList();
     static ArrayList<Voucher> voucher = new ArrayList();
+    static Scanner s = new Scanner (System.in);
+    
+    public void insertSellerItem (String itemName, int price, int stocks, String category, int itemWeight) {
+        System.out.println("Masukkan kembali username: ");
+        String username = s.next();
+        new ControllerAddItem().insertSellerItem(seller, username, itemName, price, stocks, category, itemWeight);
+    }
 }
