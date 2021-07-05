46afc32e2d569af6329032f41d3c12f3759ed8fb
Intel <Intel@Trench>
2021-07-05 07:22:24 +0700

    Edit Controller dan Menu Voucher

--- src/controller/ControllerVoucher.java
+++ src/controller/ControllerVoucher.java
@@ -8,8 +8,10 @@ package controller;
 import static controller.ControllerFlashsale.conn;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import model.Voucher;
 
 /**
@@ -40,18 +42,40 @@ public class ControllerVoucher {
         }
     }
 
-    public void seeVoucher() {
-        
+    public ArrayList<Voucher> getVoucherDataSeller() {
+        conn.connect();
+        String query = "SELECT * FROM voucher WHERE idPerson='" + MainController.activeID + "'";
+        ArrayList<Voucher> arrVoucher = new ArrayList();
+
+        try {
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                Voucher newVoucher = new Voucher();
+                newVoucher.setIdVoucher(rs.getInt("idVoucher"));
+                newVoucher.setMinTransaction(rs.getInt("minTransaction"));
+                newVoucher.setCashback(rs.getInt("cashback"));
+                newVoucher.setVoucherCode(rs.getString("voucherCode"));
+                String date = rs.getDate("endDate").toString();
+                newVoucher.setEndDate(date);
+                newVoucher.setIsAvailable(rs.getInt("isAvailable"));
+                arrVoucher.add(newVoucher);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return arrVoucher;
     }
 
     public void editVoucher(Voucher editVoucher) {
         conn.connect();
         String str = editVoucher.getEndDate();
         Date date = Date.valueOf(str);
-        
+
         String query = "UPDATE voucher SET minTransaction='" + editVoucher.getMinTransaction() + "', "
                 + "cashback='" + editVoucher.getCashback() + "', "
-                + "voucherCode='" + editVoucher.getVoucherCode()+ "', "
+                + "voucherCode='" + editVoucher.getVoucherCode() + "', "
                 + "endDate='" + date + "', "
                 + "isAvailable='" + editVoucher.getIsAvailable() + "' "
                 + "WHERE idVoucher='" + editVoucher.getIdVoucher() + "'";
@@ -72,6 +96,55 @@ public class ControllerVoucher {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public ArrayList<Voucher> getVoucherDataNonSeller() {
+        conn.connect();
+        String query = "SELECT * FROM voucher";
+        ArrayList<Voucher> arrVoucher = new ArrayList();
+
+        try {
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                Voucher newVoucher = new Voucher();
+                newVoucher.setIdVoucher(rs.getInt("idVoucher"));
+                newVoucher.setMinTransaction(rs.getInt("minTransaction"));
+                newVoucher.setCashback(rs.getInt("cashback"));
+                newVoucher.setVoucherCode(rs.getString("voucherCode"));
+                String date = rs.getDate("endDate").toString();
+                newVoucher.setEndDate(date);
+                newVoucher.setIsAvailable(rs.getInt("isAvailable"));
+                arrVoucher.add(newVoucher);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return arrVoucher;
+    }
+
+    public Voucher getVoucherDataById(int idVoucher) {
+        conn.connect();
+        String query = "SELECT * FROM voucher WHERE idVoucher='" + idVoucher + "'";
+        Voucher voucher = new Voucher();
+        try {
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                voucher.setIdVoucher(rs.getInt("idVoucher"));
+                voucher.setMinTransaction(rs.getInt("minTransaction"));
+                voucher.setCashback(rs.getInt("cashback"));
+                voucher.setVoucherCode(rs.getString("voucherCode"));
+                String date = rs.getDate("endDate").toString();
+                voucher.setEndDate(date);
+                voucher.setIsAvailable(rs.getInt("isAvailable"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return voucher;
     }
 
 }
